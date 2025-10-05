6bc9183d5e7939d13a2b0e15fb64aba025b5315c
Syed Imadulla <imadulla.s45@gmail.com>
2025-10-05 10:17:29 +0530

    Create pattern2.c

---
+++ pattern2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    // Outer loop for printing all row
+    for (int i = 0; i < rows; i++) {
+
+        // First inner loop printing
+        // leading white spaces
+        for (int j = 0; j < 2 * i; j++) {
+            printf(" ");
+        }
+
+        // Second inner loop for 
+        // printing alphabets
+        for (int k = 0; k < 2 * (rows - i) - 1; k++) {
+            printf("%c ", 'A' + k);
+        }
+        printf("\n");
+    }
+}
